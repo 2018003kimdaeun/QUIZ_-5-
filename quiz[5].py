2ee1c279488385113a624aefe993962753e13d7c
2018003kimdaeun <144420895+2018003kimdaeun@users.noreply.github.com>
2023-10-06 16:53:07 +0900

    Add files via upload

--- quiz[5].py
+++ quiz[5].py
@@ -1,3 +1,15 @@
-var = input("몇 단까지 출력할까요? ")
-if table > 20:
-        print("20 이하의 숫자를 입력해주세요.")
+var = int(input("몇 단까지 출력할까요? "))
+print(var, type(var))
+
+def gugudan(table):
+    print("구구단을 출력합니다.")
+    if table < 1:
+        print("1 이상의 숫자를 입력해주세요.")
+        return
+    for x in range(1, table + 1):
+        print("------[" + str(x) + "단]------")
+        for y in range(1, table + 1):
+            print(f"{x} x {y} = {x * y}")
+
+gugudan(var)
+
